e28fe704a41d460f251f50d1a01bfaf71de1470c
KevinMisati <kevinmosigisi2001@gmail.com>
2024-12-03 06:31:46 +0300

    adding the save resumes actions

--- src/app/(main)/editor/actions.ts
+++ src/app/(main)/editor/actions.ts
@@ -1,8 +1,78 @@
 "use server"
 
-import { ResumeValues } from "@/lib/validation";
+import prisma from "@/lib/prisma";
+import { resumeSchema, ResumeValues } from "@/lib/validation";
+import { auth } from "@clerk/nextjs/server";
+import { del, put } from "@vercel/blob";
+import path from "path";
 
 export default async function saveResume(values: ResumeValues){
     const {id} = values
     
+    console.log("received values",values)
+
+    const {
+        photo,workExperiences,educations,...resumeValues
+    } = resumeSchema.parse(values)
+
+    const {userId} = await auth()
+
+    if(!userId){
+        throw new Error("User not authenticated")
+    }
+
+    const existingResume = id
+      ? await prisma.resume.findUnique({ where: { id, userId } })
+      : null;
+
+    if(id && !existingResume){
+        throw new  Error("Resume not found")
+    }
+
+    let newPhotoUrl:String | undefined | null = undefined
+
+    if(photo instanceof File){
+        if(existingResume?.photourl){
+            await del(existingResume.photourl)
+        }
+
+        const blob = await put(`resume_photos/${path.extname(photo.name)}`,photo,{
+            access:"public"
+        })
+        newPhotoUrl = blob.url
+    }
+    else if(photo === null){
+        if (existingResume?.photourl) {
+          await del(existingResume.photourl);
+        }
+        newPhotoUrl = null
+    }
+    if (id) {
+    return prisma.resume.update({
+      where: { id },
+      data: {
+        ...resumeValues,
+        photoUrl: newPhotoUrl,
+        workExperiences: {
+          deleteMany: {},
+          create: workExperiences?.map((exp) => ({
+            ...exp,
+            startDate: exp.startDate ? new Date(exp.startDate) : undefined,
+            endDate: exp.endDate ? new Date(exp.endDate) : undefined,
+          })),
+        },
+        educations: {
+          deleteMany: {},
+          create: educations?.map((edu) => ({
+            ...edu,
+            startDate: edu.startDate ? new Date(edu.startDate) : undefined,
+            endDate: edu.endDate ? new Date(edu.endDate) : undefined,
+          })),
+        },
+        updatedAt: new Date(),
+      },
+    });
+    else{
+
+    }
 }
